7419c98bb5891b0ee12045087299abd8833cd2d5
CompSciHigh24 <csh-github@compscihigh.org>
2026-01-09 12:11:17 -0500

    Update instruction comments for server setup

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // INSTRUCTIONS: Write the code that does the following:
-// -1 Set up the server correctly
-// -2 console logs the method and URL for every request
-// -3 sends back "Welcome to the Animal Shelter Network" as an h1 when the client goes to /
-// -4 sends back the cat object when the client goes to /api/cat
-// -5 sends back the shelters object when the client goes to /api/shelters
-// -6 sends back "Go to /api/cat to see cats for adoption and /api/shelters to see shelters in the area" when the client goes to /docs
-// -7 BLUE & PINK: sends back "A cat you can adopt is " with the name of the first cat when the client goes to /adopt/cat 
+// -0 Set up the server correctly
+// -1 console logs the method and URL for every request
+// -2 sends back "Welcome to the Animal Shelter Network" as an h1 when the client goes to /
+// -3 sends back the cat object when the client goes to /api/cat
+// -4 sends back the shelters object when the client goes to /api/shelters
+// -5 sends back "Go to /api/cat to see cats for adoption and /api/shelters to see shelters in the area" when the client goes to /docs
+// -6 BLUE & PINK: sends back "A cat you can adopt is " with the name of the first cat when the client goes to /adopt/cat 
 //    (Use dot notation & string concatenation)
-// -8 sends back a 404 page for all other paths
+// -7 sends back a 404 page for all other paths
 
 // PINK Only: Add the correct status codes to ALL route handlers
 
